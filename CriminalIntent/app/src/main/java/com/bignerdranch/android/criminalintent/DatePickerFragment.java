62b67876ac477d3812a6790dbc3806d44a1f2e4b
Sean Lao <slao258@gmail.com>
2015-12-27 07:32:58 -0800

    Add alert dialog for date picker

---
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
@@ -0,0 +1,19 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+
+/**
+ * Created by slao on 12/27/15.
+ */
+public class DatePickerFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.date_picker_title)
+                .setPositiveButton(android.R.string.ok, null)
+                .create();
+    }
+}
