4e8bcdb541c72d8fc862ff1acba085623bb5136d
Sean Lao <slao258@gmail.com>
2016-01-07 08:11:01 -0800

    add choose suspect and send report

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -11,6 +11,7 @@ public class Crime {
     private String mTitle;
     private Date mDate;
     private boolean mSolved;
+    private String mSuspect;
 
     public Crime() {
         // Generate unique identifier
@@ -48,5 +49,13 @@ public class Crime {
 
     public void setSolved(boolean solved) {
         mSolved = solved;
+    }
+
+    public String getSuspect() {
+        return mSuspect;
+    }
+
+    public void setSuspect(String suspect) {
+        mSuspect = suspect;
     }
 }
