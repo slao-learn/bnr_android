d61c34986e12e1a0922da29b4a6425ddd25dd7ed
Sean Lao <slao258@gmail.com>
2016-01-17 23:58:00 -0800

    BeatBox: play sounds

--- BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/Sound.java
+++ BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/Sound.java
@@ -6,6 +6,7 @@ package com.bignerdranch.android.beatbox;
 public class Sound {
     private String mAssetPath;
     private String mName;
+    private Integer mSoundId;
 
     public Sound(String assetPath) {
         mAssetPath = assetPath;
@@ -20,6 +21,14 @@ public class Sound {
 
     public String getName() {
         return mName;
+    }
+
+    public Integer getSoundId() {
+        return mSoundId;
+    }
+
+    public void setSoundId(Integer soundId) {
+        mSoundId = soundId;
     }
 }
 
